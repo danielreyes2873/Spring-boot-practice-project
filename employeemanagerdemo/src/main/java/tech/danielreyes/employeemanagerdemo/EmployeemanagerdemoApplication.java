7c78cc742fb4576341e45e25132113b93084cf13
Daniel Reyes <daniereyes@csumb.edu>
2022-07-12 14:33:06 -0700

    Added initial project files

---
+++ employeemanagerdemo/src/main/java/tech/danielreyes/employeemanagerdemo/EmployeemanagerdemoApplication.java
@@ -0,0 +1,13 @@
+package tech.danielreyes.employeemanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerdemoApplication.class, args);
+	}
+
+}
